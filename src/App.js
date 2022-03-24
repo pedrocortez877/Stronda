1bb3f97b20c499ca0a468be75b3d8ac33ae3d490
Pedro Cortez <pedrocortez877@gmail.com>
2022-03-23 22:28:25 -0300

    Criando rota de venda de produto

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import { Operations } from "./pages/Operations";
+import { ProductSale } from "./pages/ProductSale";
 
 function App() {
-  return(
+  return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Operations />} />
+        <Route path="/productSale" element={<ProductSale />} />
       </Routes>
     </BrowserRouter>
   );
