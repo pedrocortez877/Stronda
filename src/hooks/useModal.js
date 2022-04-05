500b4ac253fac8fa51a2e88f1e29e44b05933ba7
Pedro Cortez <pedrocortez877@gmail.com>
2022-04-04 22:37:24 -0300

    feat: criando hook use modal

---
+++ src/hooks/useModal.js
@@ -0,0 +1,10 @@
+import { useState } from "react";
+
+export function useModal() {
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  return [open, handleOpen, handleClose];
+}
