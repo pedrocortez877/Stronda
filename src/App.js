84b599b080312f6c38f4b88f724b0a13d502f681
Pedro Cortez <pedrocortez877@gmail.com>
2022-05-10 22:39:33 -0300

    fix: corrigindo state de produtos de uma venda

--- src/App.js
+++ src/App.js
@@ -3,12 +3,21 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Operations } from "./pages/Operations";
 import { ProductSale } from "./pages/ProductSale";
 
+import { ProductOfASaleContextProvider } from "./contexts/ProductOfASaleContext";
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Operations />} />
-        <Route path="/productSale" element={<ProductSale />} />
+        <Route
+          path="/productSale"
+          element={
+            <ProductOfASaleContextProvider>
+              <ProductSale />
+            </ProductOfASaleContextProvider>
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
