3ef63b42950e98307f9d06d1da28f429bc9c68c3
More Dev <pedrocortez877@gmail.com>
2022-06-28 20:25:21 -0300

    feat: create context Product Sale

---
+++ src/contexts/ProductSaleContext.js
@@ -0,0 +1,21 @@
+import { createContext, useState } from "react";
+
+const ProductSaleContext = createContext({});
+
+export function ProductSaleContextProvider({ children }) {
+  const [totalValue, setTotalValue] = useState(0);
+
+  function changeTotalValue(value) {
+    setTotalValue(oldValue => oldValue + Number(value));
+  }
+
+  return (
+    <ProductSaleContext.Provider
+      value={{ changeTotalValue }}
+    >
+      {children}
+    </ProductSaleContext.Provider>
+  );
+}
+
+export default ProductSaleContext;
